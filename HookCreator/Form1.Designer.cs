bdd9830f297c45d1faea18e7e62b81fa6c975fac
Bruno13 <bruno.travit@gmail.com>
2017-12-30 15:18:34 -0200

    Versão inicial

---
+++ HookCreator/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace HookCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxCallbacks = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxHookPrefix = new System.Windows.Forms.TextBox();
+            this.buttonMakeHook = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButtonAll = new System.Windows.Forms.RadioButton();
+            this.radioButtonALS = new System.Windows.Forms.RadioButton();
+            this.radioButtonChain = new System.Windows.Forms.RadioButton();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonCopyResult = new System.Windows.Forms.Button();
+            this.radioButtonCallChain = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Callback";
+            // 
+            // comboBoxCallbacks
+            // 
+            this.comboBoxCallbacks.FormattingEnabled = true;
+            this.comboBoxCallbacks.Location = new System.Drawing.Point(29, 29);
+            this.comboBoxCallbacks.Name = "comboBoxCallbacks";
+            this.comboBoxCallbacks.Size = new System.Drawing.Size(375, 21);
+            this.comboBoxCallbacks.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hook Prefix";
+            // 
+            // textBoxHookPrefix
+            // 
+            this.textBoxHookPrefix.Location = new System.Drawing.Point(29, 70);
+            this.textBoxHookPrefix.Name = "textBoxHookPrefix";
+            this.textBoxHookPrefix.Size = new System.Drawing.Size(375, 20);
+            this.textBoxHookPrefix.TabIndex = 3;
+            this.textBoxHookPrefix.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxHookPrefix_KeyPress);
+            // 
+            // buttonMakeHook
+            // 
+            this.buttonMakeHook.Location = new System.Drawing.Point(16, 97);
+            this.buttonMakeHook.Name = "buttonMakeHook";
+            this.buttonMakeHook.Size = new System.Drawing.Size(388, 33);
+            this.buttonMakeHook.TabIndex = 4;
+            this.buttonMakeHook.Text = "GENERATE";
+            this.buttonMakeHook.UseVisualStyleBackColor = true;
+            this.buttonMakeHook.Click += new System.EventHandler(this.buttonMakeHook_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // radioButtonAll
+            // 
+            this.radioButtonAll.AutoSize = true;
+            this.radioButtonAll.Location = new System.Drawing.Point(59, 135);
+            this.radioButtonAll.Name = "radioButtonAll";
+            this.radioButtonAll.Size = new System.Drawing.Size(36, 17);
+            this.radioButtonAll.TabIndex = 6;
+            this.radioButtonAll.TabStop = true;
+            this.radioButtonAll.Text = "All";
+            this.radioButtonAll.UseVisualStyleBackColor = true;
+            this.radioButtonAll.CheckedChanged += new System.EventHandler(this.radioButtonAll_CheckedChanged);
+            // 
+            // radioButtonALS
+            // 
+            this.radioButtonALS.AutoSize = true;
+            this.radioButtonALS.Location = new System.Drawing.Point(92, 135);
+            this.radioButtonALS.Name = "radioButtonALS";
+            this.radioButtonALS.Size = new System.Drawing.Size(69, 17);
+            this.radioButtonALS.TabIndex = 7;
+            this.radioButtonALS.TabStop = true;
+            this.radioButtonALS.Text = "Only ALS";
+            this.radioButtonALS.UseVisualStyleBackColor = true;
+            this.radioButtonALS.CheckedChanged += new System.EventHandler(this.radioButtonALS_CheckedChanged);
+            // 
+            // radioButtonChain
+            // 
+            this.radioButtonChain.AutoSize = true;
+            this.radioButtonChain.Location = new System.Drawing.Point(158, 135);
+            this.radioButtonChain.Name = "radioButtonChain";
+            this.radioButtonChain.Size = new System.Drawing.Size(76, 17);
+            this.radioButtonChain.TabIndex = 8;
+            this.radioButtonChain.TabStop = true;
+            this.radioButtonChain.Text = "Only Chain";
+            this.radioButtonChain.UseVisualStyleBackColor = true;
+            this.radioButtonChain.CheckedChanged += new System.EventHandler(this.radioButtonChain_CheckedChanged);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(19, 158);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxResult.Size = new System.Drawing.Size(385, 136);
+            this.textBoxResult.TabIndex = 9;
+            this.textBoxResult.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxResult_KeyDown);
+            // 
+            // buttonCopyResult
+            // 
+            this.buttonCopyResult.Location = new System.Drawing.Point(18, 299);
+            this.buttonCopyResult.Name = "buttonCopyResult";
+            this.buttonCopyResult.Size = new System.Drawing.Size(387, 34);
+            this.buttonCopyResult.TabIndex = 10;
+            this.buttonCopyResult.Text = "COPY RESULT";
+            this.buttonCopyResult.UseVisualStyleBackColor = true;
+            this.buttonCopyResult.Click += new System.EventHandler(this.buttonCopyResult_Click);
+            // 
+            // radioButtonCallChain
+            // 
+            this.radioButtonCallChain.AutoSize = true;
+            this.radioButtonCallChain.Location = new System.Drawing.Point(231, 135);
+            this.radioButtonCallChain.Name = "radioButtonCallChain";
+            this.radioButtonCallChain.Size = new System.Drawing.Size(142, 17);
+            this.radioButtonCallChain.TabIndex = 11;
+            this.radioButtonCallChain.TabStop = true;
+            this.radioButtonCallChain.Text = "Only Callback with Chain";
+            this.radioButtonCallChain.UseVisualStyleBackColor = true;
+            this.radioButtonCallChain.CheckedChanged += new System.EventHandler(this.radioButtonCallChain_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 339);
+            this.Controls.Add(this.buttonCopyResult);
+            this.Controls.Add(this.radioButtonCallChain);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.radioButtonChain);
+            this.Controls.Add(this.radioButtonALS);
+            this.Controls.Add(this.radioButtonAll);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonMakeHook);
+            this.Controls.Add(this.textBoxHookPrefix);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxCallbacks);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hook Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxHookPrefix;
+        private System.Windows.Forms.Button buttonMakeHook;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButtonAll;
+        private System.Windows.Forms.RadioButton radioButtonALS;
+        private System.Windows.Forms.RadioButton radioButtonChain;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.ComboBox comboBoxCallbacks;
+        private System.Windows.Forms.Button buttonCopyResult;
+        private System.Windows.Forms.RadioButton radioButtonCallChain;
+    }
+}
+
---
+++ HookCreator/Form1.cs
@@ -0,0 +1,372 @@
+﻿/*
+	Hook Creator(Criador de Hook)
+		- Simplifica o trabalho para criar o hook (method ALS 7) de uma
+        função, oferecendo a lista de todas as callbacks do SA-MP com
+        todos seus parâmetros. Também disponibiliza a opção para separar
+        as partes do hook para fácil manejo.
+
+		Versão: 1.0.0
+		Última atualização: 30/12/2017
+
+	Copyright (C) 2017 Bruno "Bruno13" Travi.
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Esqueleto do código:
+	|
+	 *
+	 * LIBRARIES
+	 *
+	|
+	 *
+	 * ARRAYS
+	 *
+	|
+	 *
+	 * CALLBACKS
+	 *
+	|
+	 *
+	 * EVENTS
+	 *
+	|
+*/
+/*
+ * LIBRARIES
+ ******************************************************************************
+ */
+using System;
+using System.Windows.Forms;
+
+namespace HookCreator
+{
+    public partial class Form1 : Form
+    {
+        /*
+         * ARRAYS
+         ******************************************************************************
+         */
+        string[] callbacks = new string[]
+        {
+            "OnActorStreamIn",
+            "OnActorStreamOut",
+            "OnClientMessage",
+            "OnDialogResponse",
+            "OnEnterExitModShop",
+            "OnFilterScriptExit",
+            "OnFilterScriptInit",
+            "OnGameModeExit",
+            "OnGameModeInit",
+            "OnIncomingConnection",
+            "OnObjectMoved",
+            "OnPlayerClickMap",
+            "OnPlayerClickPlayer",
+            "OnPlayerClickPlayerTextDraw",
+            "OnPlayerClickTextDraw",
+            "OnPlayerCommandText",
+            "OnPlayerConnect",
+            "OnPlayerDeath",
+            "OnPlayerDisconnect",
+            "OnPlayerEditAttachedObject",
+            "OnPlayerEditObject",
+            "OnPlayerEnterCheckpoint",
+            "OnPlayerEnterRaceCheckpoint",
+            "OnPlayerEnterVehicle",
+            "OnPlayerExitVehicle",
+            "OnPlayerExitedMenu",
+            "OnPlayerGiveDamage",
+            "OnPlayerGiveDamageActor",
+            "OnPlayerInteriorChange",
+            "OnPlayerKeyStateChange",
+            "OnPlayerLeaveCheckpoint",
+            "OnPlayerLeaveRaceCheckpoint",
+            "OnPlayerObjectMoved",
+            "OnPlayerPickUpPickup",
+            "OnPlayerRequestClass",
+            "OnPlayerRequestSpawn",
+            "OnPlayerSelectObject",
+            "OnPlayerSelectedMenuRow",
+            "OnPlayerSpawn",
+            "OnPlayerStateChange",
+            "OnPlayerStreamIn",
+            "OnPlayerStreamOut",
+            "OnPlayerTakeDamage",
+            "OnPlayerText",
+            "OnPlayerUpdate",
+            "OnPlayerWeaponShot",
+            "OnRconCommand",
+            "OnRconLoginAttempt",
+            "OnRecordingPlaybackEnd",
+            "OnTrailerUpdate",
+            "OnUnoccupiedVehicleUpdate",
+            "OnVehicleDamageStatusUpdate",
+            "OnVehicleDeath",
+            "OnVehicleMod",
+            "OnVehiclePaintjob",
+            "OnVehicleRespray",
+            "OnVehicleSirenStateChange",
+            "OnVehicleSpawn",
+            "OnVehicleStreamIn",
+            "OnVehicleStreamOut"
+        },
+
+        callbacksParams = new string[]
+        {
+            "(actorid, forplayerid)",
+            "(actorid, forplayerid)",
+            "(color, text[])",
+            "(playerid, dialogid, response, listitem, inputtext[])",
+            "(playerid, enterexit, interiorid)",
+            "()",
+            "()",
+            "()",
+            "()",
+            "(playerid, ip_address[], port)",
+            "(objectid)",
+            "(playerid, Float:fX, Float:fY, Float:fZ)",
+            "(playerid, clickedplayerid, source)",
+            "(playerid, PlayerText:playertextid)",
+            "(playerid, Text:clickedid)",
+            "(playerid, cmdtext[])",
+            "(playerid)",
+            "(playerid, killerid, reason)",
+            "(playerid, reason)",
+            "(playerid, response, index, modelid, boneid, Float:fOffsetX, Float:fOffsetY, Float:fOffsetZ, Float:fRotX, Float:fRotY, Float:fRotZ, Float:fScaleX, Float:fScaleY, Float:fScaleZ)",
+            "(playerid, playerobject, objectid, response, Float:fX, Float:fY, Float:fZ, Float:fRotX, Float:fRotY, Float:fRotZ)",
+            "(playerid)",
+            "(playerid)",
+            "(playerid, vehicleid, ispassenger)",
+            "(playerid, vehicleid)",
+            "(playerid)",
+            "(playerid, damagedid, Float:amount, weaponid, bodypart)",
+            "(playerid, damaged_actorid, Float:amount, weaponid, bodypart)",
+            "(playerid, newinteriorid, oldinteriorid)",
+            "(playerid, newkeys, oldkeys)",
+            "(playerid)",
+            "(playerid)",
+            "(playerid, objectid)",
+            "(playerid, pickupid)",
+            "(playerid, classid)",
+            "(playerid)",
+            "(playerid, type, objectid, modelid, Float:fX, Float:fY, Float:fZ)",
+            "(playerid, row)",
+            "(playerid)",
+            "(playerid, newstate, oldstate)",
+            "(playerid, forplayerid)",
+            "(playerid, forplayerid)",
+            "(playerid, issuerid, Float:amount, weaponid, bodypart)",
+            "(playerid, text[])",
+            "(playerid)",
+            "(playerid, weaponid, hittype, hitid, Float:fX, Float:fY, Float:fZ)",
+            "(cmd[])",
+            "(ip[], password[], success)",
+            "()",
+            "(playerid, vehicleid)",
+            "(vehicleid, playerid, passenger_seat, Float:new_x, Float:new_y, Float:new_z, Float:vel_x, Float:vel_y, Float:vel_z)",
+            "(vehicleid, playerid)",
+            "(vehicleid, killerid)",
+            "(playerid, vehicleid, componentid)",
+            "(playerid, vehicleid, paintjobid)",
+            "(playerid, vehicleid, color1, color2)",
+            "(playerid, vehicleid, newstate)",
+            "(vehicleid)",
+            "(vehicleid, forplayerid)",
+            "(vehicleid, forplayerid)"
+        },
+            
+        charParamsRemove = new string[]
+        {
+            "[]",
+            "Float:"
+        };
+
+        string hookAllFormat = "public Function(Args)\r\n{\r\n\t#if defined Prefix_Function\r\n\t\tPrefix_Function(Args2);\r\n\t#endif\r\n}\r\n#if defined _ALS_Function\r\n\t#undef Function\r\n#else\r\n\t#define _ALS_Function\r\n#endif\r\n#define Function Prefix_Function\r\n#if defined Prefix_Function\r\n\tforward Prefix_Function(Args);\r\n#endif";
+        string hookALSFormat = "#if defined _ALS_Function\r\n\t#undef Function\r\n#else\r\n\t#define _ALS_Function\r\n#endif\r\n#define Function Prefix_Function\r\n#if defined Prefix_Function\r\n\tforward Prefix_Function(Args);\r\n#endif";
+        string hookChainFormat = "\t#if defined Prefix_Function\r\n\t\tPrefix_Function(Args2);\r\n\t#endif";
+        string hookCallChainFormat = "public Function(Args)\r\n{\r\n\t#if defined Prefix_Function\r\n\t\tPrefix_Function(Args2);\r\n\t#endif\r\n}";
+        string hookAllFormatCompALS = "public Function(Args)\r\n{\r\n\t#if defined Prefix_Function\r\n\t\tPrefix_Function(Args2);\r\n\t#endif\r\n}\r\n#if defined ALS_Function\r\n\t#undef Function\r\n#else\r\n\t#define ALS_Function\r\n#endif\r\n#define Function Prefix_Function\r\n#if defined Prefix_Function\r\n\tforward Prefix_Function(Args);\r\n#endif";
+        string hookALSFormatCompALS = "#if defined ALS_Function\r\n\t#undef Function\r\n#else\r\n\t#define ALS_Function\r\n#endif\r\n#define Function Prefix_Function\r\n#if defined Prefix_Function\r\n\tforward Prefix_Function(Args);\r\n#endif";
+        
+        /*
+         * CALBLACKS
+         ******************************************************************************
+         */
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadCheckBoxCallbacks();
+
+            radioButtonAll.Checked = true;
+            radioButtonALS.Checked = radioButtonChain.Checked = radioButtonCallChain.Checked = false;
+
+            Select();
+        }
+
+        /// <summary>
+        /// Insere todas as callbacks da matriz 'callbacks' nos itens do comboBoxCallbacks.
+        /// <returns>Não retorna valores.</returns>
+        /// </summary>
+        private void LoadCheckBoxCallbacks()
+        {
+            for (int i = 0; i < callbacks.Length; i++)
+                comboBoxCallbacks.Items.Add(callbacks[i]);
+
+            comboBoxCallbacks.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Cria o hook e insere o resultado na textBoxResult.
+        /// <param name="warnErrors">Se true, avisa erros de preenchimento do textBoxHookPrefix.</param>
+        /// <returns>Não retorna valores.</returns>
+        /// </summary>
+        private void CreateHookResult(bool warnErrors = false)
+        {
+            int maxChar = 0;
+
+            if (textBoxHookPrefix.Text == "")
+            {
+                if (warnErrors)
+                    MessageBox.Show("Field Hook Prefix empty!");
+
+                return;
+            }
+            else if (!IsValidHookPrefix(textBoxHookPrefix.Text, ref maxChar))
+            {
+                if (warnErrors)
+                    MessageBox.Show((string.Format("Hook Prefix can't be longer than {0} characters!", maxChar)));
+
+                return;
+            }
+
+            int callbackSelected = comboBoxCallbacks.SelectedIndex, indexChar = 0;
+
+            bool isALSComp = (callbacks[callbackSelected].Length > 26);
+
+            string output, callParams;
+
+            if (radioButtonAll.Checked)
+                output = (isALSComp) ? (hookAllFormatCompALS) : (hookAllFormat);
+            else if (radioButtonALS.Checked)
+                output = (isALSComp) ? (hookALSFormatCompALS) : (hookALSFormat);
+            else if (radioButtonChain.Checked)
+                output = hookChainFormat;
+            else
+                output = hookCallChainFormat;
+
+            callParams = callbacksParams[callbackSelected];
+
+            while (indexChar != -1)
+            {
+                indexChar = callParams.IndexOf(charParamsRemove[0]);
+
+                if (indexChar != -1)
+                    callParams = callParams.Remove(indexChar, 2);
+            }
+
+            indexChar = 0;
+
+            while (indexChar != -1)
+            {
+                indexChar = callParams.IndexOf(charParamsRemove[1]);
+
+                if (indexChar != -1)
+                    callParams = callParams.Remove(indexChar, 6);
+            }
+
+            output = output.Replace("Function", callbacks[callbackSelected]);
+
+            output = output.Replace("(Args)", callbacksParams[callbackSelected]);
+
+            output = output.Replace("(Args2)", callParams);
+
+            output = output.Replace("Prefix", textBoxHookPrefix.Text);
+
+            textBoxResult.Text = output.ToString();
+        }
+
+        /// <summary>
+        /// Valida prefixo de hook específico é válido.
+        /// <param name="prefix">Prefixo do hook.</param>
+        /// <param name="maxChar">Variável para referenciada o máximo de caracteres que o prefixo pode ter para callback selecionada.</param>
+        /// <returns>True se for válido, False se não.</returns>
+        /// </summary>
+        private bool IsValidHookPrefix(string prefix, ref int maxChar)
+        {
+            int callbackSelected = comboBoxCallbacks.SelectedIndex,
+                callbackNameSize = callbacks[callbackSelected].Length + 1;//+1 = _
+
+            maxChar = 31 - callbackNameSize;
+
+            return ((prefix.Length + callbackNameSize) <= 31);
+        }
+
+        /*
+         * EVENTS
+         ******************************************************************************
+         */
+        private void buttonMakeHook_Click(object sender, System.EventArgs e)
+        {
+            CreateHookResult(true);
+        }
+
+        private void buttonCopyResult_Click(object sender, EventArgs e)
+        {
+            if (textBoxResult.Text != "")
+                Clipboard.SetText(textBoxResult.Text);
+        }
+
+        private void textBoxResult_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.A)
+            {
+                if (sender != null)
+                {
+                    ((TextBox)sender).SelectAll();
+                    e.Handled = true;
+                    e.SuppressKeyPress = true;
+                }
+            }
+        }
+
+        private void textBoxHookPrefix_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((Keys)e.KeyChar == Keys.Enter)
+            {
+                CreateHookResult(true);
+                e.Handled = true;
+            }
+        }
+
+        private void radioButtonAll_CheckedChanged(object sender, EventArgs e)
+        {
+            CreateHookResult();
+        }
+
+        private void radioButtonALS_CheckedChanged(object sender, EventArgs e)
+        {
+            CreateHookResult();
+        }
+
+        private void radioButtonChain_CheckedChanged(object sender, EventArgs e)
+        {
+            CreateHookResult();
+        }
+
+        private void radioButtonCallChain_CheckedChanged(object sender, EventArgs e)
+        {
+            CreateHookResult();
+        }
+    }
+}
